76e59401c8d888cc77191baab83f9563a08355df
Road-block <dridzt.addons@hotmail.com>
2024-01-08 06:03:01 +0200

    add info to shipments (filled supplies) as well: rep, real xp, money (including xp>money conversion) add tooltip cues to link tooltips in addition to item tooltips prompt to turnin shipments in your possession (bags or bank) to supply officer when visiting a town with one optionally set an arrow to the supply officer if TomTom is installed update metadata

--- core.lua
+++ core.lua
@@ -5,11 +5,17 @@ local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_LOGIN") -- faction's not going to change during a play session, no reason to call it every tooltip show
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:RegisterEvent("UPDATE_FACTION") -- rep standing on the other hand can, but let's update only on relevant events
+f:RegisterEvent("PLAYER_LEVEL_UP") -- xp to money conversion
+f:RegisterEvent("ZONE_CHANGED_NEW_AREA") -- turnin alert
+f:RegisterEvent("ADDON_LOADED")
 f.OnEvent = function(_,event,...)
   return addon[event] and addon[event](addon,...)
 end
 f:SetScript("OnEvent", f.OnEvent)
 
+local COLOR_STRONG_CYAN = CreateColor(0,198/255,209/255)
+addon.label = COLOR_STRONG_CYAN:WrapTextInColorCode(addonName)
+addon.short = COLOR_STRONG_CYAN:WrapTextInColorCode("WtH")
 local FACTION_NEUTRAL, FACTION_FRIENDLY, FACTION_HONORED, FACTION_REVERED, FACTION_EXALTED = 4,5,6,7,8
 local FACTION_AZEROTH_COMMERCE, FACTION_DUROTAR_SUPPLY = 2586, 2587
 
@@ -67,10 +73,73 @@ local Supplies = {
   [211826] = { 800, 200, 3000, 500, { 14, 15869} }, --"Waylaid Supplies: Silver Skeleton Keys"
   -- Future phases
 }
+-- questlevel, rep, money, exp
+local Filled = {
+  -- Phase 1
+  [211365] = { 9, 300, 600, 105 },
+  [211367] = { 12, 450, 1500, 120 },
+  [211839] = { 18, 500, 1500, 195 },
+  [211840] = { 22, 650, 2000, 240 },
+  [211841] = { 25, 800, 3000, 270 },
+}
+-- name, npcid, mapx, mapy
+local factionNPCS = {
+  [FACTION_AZEROTH_COMMERCE] = {
+    [1453] = {L["Elaine Compton <Supply Officer>"], 213077, 54.8, 61.2}, -- Stormwind
+    [1455] = {L["Tamelyn Aldridge <Supply Officer>"], 214099, 24.6, 67.6}, -- Ironforge
+    [1457] = {L["Marcy Baker <Supply Officer>"], 214101, 59.8, 56.6}, -- Darnassus
+  },
+  [FACTION_DUROTAR_SUPPLY] = {
+    [1456] = {L["Dokimi <Supply Officer>"], 214096, 39.6, 53.6}, -- Thunder Bluff
+    [1454] = {L["Jornah <Supply Officer>"], 214070, 51.6, 63.8}, -- Orgrimmar
+    [1458] = {L["Gishah <Supply Officer>"], 214098, 65.6, 38.2}, -- Undercity
+  },
+}
+
+local tomtomOpt = {
+  desc = L["A Full Shipment"],
+  title = L["Supply Officer"],
+  from = addonName,
+  persistent = false,
+  minimap = true,
+  world = false,
+  crazy = true,
+  cleardistance = 10,
+  arrivaldistance = 5,
+}
+
+function addon:Print(msg)
+  local chatFrame = (SELECTED_CHAT_FRAME or DEFAULT_CHAT_FRAME)
+  chatFrame:AddMessage(format("%s: %s",self.short,msg))
+end
+
+function addon:Alert(mapID)
+  if mapID then
+    local turninInfo = factionNPCS[self._supplyFaction][mapID]
+    if turninInfo then
+      local supplyOfficer,npcid,map_x,map_y = unpack(turninInfo,1,4)
+      local filledSupply = self:HaveFilledSupply()
+      if filledSupply then
+        local now = GetTime()
+        if not self._lastAlert or (now-self._lastAlert) > 30 then
+          self._lastAlert = now
+          PlaySound(SOUNDKIT.UI_STORE_UNWRAP)
+          RaidNotice_AddMessage(RaidBossEmoteFrame, format(L["Turn in %s to %s"],filledSupply,supplyOfficer),ChatTypeInfo["RAID_WARNING"], 15)
+          addon:Print(format(L["Turn in %s to %s"],filledSupply,supplyOfficer))
+          if self.AddWaypoint then
+            tomtomOpt.title = supplyOfficer
+            self.AddWaypoint(TomTom,mapID,map_x/100,map_y/100,tomtomOpt)
+          end
+        end
+      end
+    end
+  end
+end
 
 function addon:CacheItems()
   self.supplyCache = self.supplyCache or {}
   self.needCache = self.needCache or {}
+  self.filledCache = self.filledCache or {}
   for k, v in pairs(Supplies) do
     local neededItem = v[5]
     local neededNum, neededItemID = unpack(neededItem,1,2)
@@ -83,6 +152,61 @@ function addon:CacheItems()
       addon.supplyCache[itemID] = {itemName,itemLevel,neededNum}
     end)
   end
+  for k,v in pairs(Filled) do
+    local itemAsync = Item:CreateFromItemID(k)
+    itemAsync:ContinueOnItemLoad(function()
+      local itemLevel = itemAsync:GetCurrentItemLevel()
+      local itemName = itemAsync:GetItemName()
+      local itemID = itemAsync:GetItemID()
+      addon.filledCache[itemID] = {itemName,itemLevel}
+    end)
+  end
+end
+
+function addon:HaveFilledSupply()
+  for item,info in pairs(Filled) do
+    local count = GetItemCount(item, true) -- include bank, we'll only trigger in town anyway
+    if count > 0 then
+      return addon.filledCache[item][1]
+    end
+  end
+  return false
+end
+
+function addon:CalculateRewards(questlevel, xp)
+  local greenlevel = self._playerLevel - GetQuestGreenRange()
+  local atlevelcap = self._playerLevel == self._playerMaxLevel
+  local extraMoney, realXP = 0, xp
+  if questlevel >= greenlevel then -- green or higher quest
+    if atlevelcap then
+      extraMoney = xp * 6
+      return 0, extraMoney
+    else
+      return xp, 0
+    end
+  else -- gray
+    local diff = greenlevel - questlevel
+    if diff > 5 then
+      realXP = 0
+    elseif diff > 4 then
+      realXP = Round(xp*0.1)
+    elseif diff > 3 then
+      realXP = Round(xp*0.2)
+    elseif diff > 2 then
+      realXP = Round(xp*0.4)
+    elseif diff > 1 then
+      realXP = Round(xp*0.6)
+    else
+      realXP = Round(xp*0.8)
+    end
+    if atlevelcap then
+      extraMoney = realXP * 6
+      return 0, extraMoney
+    else
+      return realXP, 0
+    end
+  end
+  return realXP, extraMoney
 end
 
 function addon:SupplyDetails(supplyID)
@@ -95,6 +219,17 @@ function addon:SupplyDetails(supplyID)
   return repFilled*(self._repMultiplier or 1), repUnfilled*(self._repMultiplier or 1), moneyFilled, moneyUnfilled, itemLevel, needItemPhase
 end
 
+function addon:FilledDetails(supplyID)
+  local staticInfo = Filled[supplyID]
+  if not staticInfo then return end
+  local dynamicInfo = self.filledCache[supplyID]
+  if not dynamicInfo then return end
+  local questLevel, questRep, questMoney, questExp = unpack(staticInfo,1,4)
+  local itemName, itemLevel = dynamicInfo[1], dynamicInfo[2]
+  local realExp, extraMoney = self:CalculateRewards(questLevel,questExp)
+  return questRep*(self._repMultiplier or 1), questMoney+extraMoney, realExp, itemLevel, itemName
+end
+
 function addon:AddTipInfo()
   local tooltip = self
   local self = addon
@@ -104,35 +239,53 @@ function addon:AddTipInfo()
   local repFilled, repUnfilled, moneyFilled, moneyUnfilled, itemLevel, supplyName, supplyLevel
   local numNeeded, needItemPhase
   local noRep, givesRep, filledReward, unfilledReward, usedFor
+  local questRep, questMoney, questExp, questItem
   if itemID then
     local supply = Supplies[itemID]
     local needed = addon.needCache[itemID]
+    local filled = Filled[itemID]
     if supply then
       repFilled, repUnfilled, moneyFilled, moneyUnfilled, itemLevel, needItemPhase = addon:SupplyDetails(itemID)
-    elseif needed then
-      supplyName, supplyLevel, numNeeded = addon.supplyCache[needed][1], addon.supplyCache[needed][2], addon.supplyCache[needed][3]
-    end
-    if itemLevel then
-      if itemLevel < addon._threshold then
-        noRep = RED_FONT_COLOR:WrapTextInColorCode(L["Too low for "]..addon._factionName)
-      else
-        givesRep = GREEN_FONT_COLOR:WrapTextInColorCode(addon._factionName..": ")
-        if needItemPhase and SoD_Phase < needItemPhase then
-          filledReward = RED_FONT_COLOR:WrapTextInColorCode(L["Filled: +"]..repFilled..L[" Rep, "]..GetMoneyString(moneyFilled)..format(L[" (P%d)"],needItemPhase))
+      if itemLevel then
+        if itemLevel < addon._threshold then
+          noRep = RED_FONT_COLOR:WrapTextInColorCode(L["Too low for "]..addon._factionName)
         else
-          filledReward = L["Filled: +"]..repFilled..L[" Rep, "]..GetMoneyString(moneyFilled)
+          givesRep = GREEN_FONT_COLOR:WrapTextInColorCode(addon._factionName..": ")
+          if needItemPhase and SoD_Phase < needItemPhase then
+            filledReward = RED_FONT_COLOR:WrapTextInColorCode(L["Filled: +"]..repFilled..L[" Rep, "]..GetMoneyString(moneyFilled)..format(L[" (P%d)"],needItemPhase))
+          else
+            filledReward = L["Filled: +"]..repFilled..L[" Rep, "]..GetMoneyString(moneyFilled)
+          end
+          unfilledReward = L["Unfilled: +"]..repUnfilled..L[" Rep, "]..GetMoneyString(moneyUnfilled)
         end
-        unfilledReward = L["Unfilled: +"]..repUnfilled..L[" Rep, "]..GetMoneyString(moneyUnfilled)
+        if givesRep then
+          tooltip:AddDoubleLine(givesRep..filledReward,unfilledReward)
+        elseif noRep then
+          tooltip:AddLine(noRep)
+        end
       end
-    elseif supplyName then
-      usedFor = YELLOW_FONT_COLOR:WrapTextInColorCode(format(L["Lvl %d %s (x%d)"],supplyLevel,supplyName,numNeeded))
+      return
     end
-    if givesRep then
-      tooltip:AddDoubleLine(givesRep..filledReward,unfilledReward)
-    elseif noRep then
-      tooltip:AddLine(noRep)
-    elseif usedFor then
-      tooltip:AddDoubleLine(addon._factionName,usedFor)
+    if filled then
+      questRep, questMoney, questExp, itemLevel, questItem = addon:FilledDetails(itemID)
+      if itemLevel then
+        if itemLevel < addon._threshold then
+          tooltip:AddDoubleLine(GREEN_FONT_COLOR:WrapTextInColorCode(addon._factionName),RED_FONT_COLOR:WrapTextInColorCode(L[" No Rep, "])..format(L["XP: %d, "],questExp)..GetMoneyString(questMoney))
+        else
+          tooltip:AddDoubleLine(GREEN_FONT_COLOR:WrapTextInColorCode(addon._factionName),format(L["+%d Rep, "],questRep)..format(L["XP: %d, "],questExp)..GetMoneyString(questMoney))
+        end
+      end
+      return
+    end
+    if needed then
+      supplyName, supplyLevel, numNeeded = addon.supplyCache[needed][1], addon.supplyCache[needed][2], addon.supplyCache[needed][3]
+      if supplyName then
+        usedFor = YELLOW_FONT_COLOR:WrapTextInColorCode(format(L["Lvl %d %s (x%d)"],supplyLevel,supplyName,numNeeded))
+      end
+      if usedFor then
+        tooltip:AddDoubleLine(addon._factionName,usedFor)
+      end
+      return
     end
   end
 end
@@ -151,13 +304,36 @@ function addon:SetupFaction()
     local race, raceEN, racedID = UnitRace("player")
     self._repMultiplier = (racedID and racedID == 1) and 1.1 or 1.0
   end
+  self._playerLevel = UnitLevel("player")
+  self._playerMaxLevel = GetEffectivePlayerMaxLevel()
   self:CacheItems()
+end
+
+function addon:ADDON_LOADED(...)
+  if ... == "TomTom" then
+    if not self.AddWaypoint and TomTom.AddWaypoint then
+      self.AddWaypoint = TomTom.AddWaypoint
+    end
+    if not self.RemoveWaypoint and TomTom.RemoveWaypoint then
+      self.RemoveWaypoint = TomTom.RemoveWaypoint
+    end
+  end
 end
 
 function addon:PLAYER_LOGIN()
   self:SetupFaction()
   -- install our hook
   GameTooltip:HookScript("OnTooltipSetItem", self.AddTipInfo)
+  ItemRefTooltip:HookScript("OnTooltipSetItem", self.AddTipInfo)
+  -- optional TomTom use
+  if TomTom then
+    if TomTom.AddWaypoint then
+      self.AddWaypoint = TomTom.AddWaypoint
+    end
+    if TomTom.RemoveWaypoint then
+      self.RemoveWaypoint = TomTom.RemoveWaypoint
+    end
+  end
 end
 
 function addon:PLAYER_ENTERING_WORLD(...)
@@ -165,6 +341,8 @@ function addon:PLAYER_ENTERING_WORLD(...)
   if isLogin or isReload then
     self:SetupFaction()
   end
+  local mapID = C_Map.GetBestMapForUnit("player")
+  self:Alert(mapID)
 end
 
 function addon:UPDATE_FACTION()
@@ -174,3 +352,15 @@ function addon:UPDATE_FACTION()
     self._threshold = (self._standing < FACTION_FRIENDLY) and 10 or (self._standing < FACTION_HONORED) and 25 or (self._standing < FACTION_REVERED) and 40 or (self._standing < FACTION_EXALTED) and 50 or 60
   end
 end
+
+function addon:PLAYER_LEVEL_UP()
+  self._playerLevel = UnitLevel("player")
+  self._playerMaxLevel = GetEffectivePlayerMaxLevel()
+end
+
+function addon:ZONE_CHANGED_NEW_AREA()
+  local mapID = C_Map.GetBestMapForUnit("player")
+  self:Alert(mapID)
+end
+
+--_G[addonName] = addon
